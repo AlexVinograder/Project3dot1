767f6e9dc064216d1eecbe567af3760d0aa46909
Alexandr Vinogradov <vinograder@yandex.ru>
2023-05-16 01:25:05 +0300

    Solve

--- src/BmiService.java
+++ src/BmiService.java
@@ -1,6 +1,5 @@
 public class BmiService {
-    public int calculate (double height) {
-        int weight = 98;
+    public int calculate (double height, int weight) {
         double index = (double) (height * height);
         int bmi = (int) (weight / index);
         return bmi;
--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,10 @@
 public class Main {
     public static void main(String[] args) {
 
-        BmiService service = new  BmiService();
+        BmiService service = new BmiService();
         double height = 1.87;
         int weight = 98;
-        int bmi = service.calculate(height);
+        int bmi = service.calculate(height, weight);
         System.out.println("Индекс массы тела " + bmi);
 
     }
